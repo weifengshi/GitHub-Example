6d9521f398f0ad92ff5c13f137c7a4db90254d49
Rachel Jane Morris <racheljmorris@gmail.com>
2016-12-18 23:21:13 -0600

    Sample code'

---
+++ Example Program/example.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    // Variable declarations
+    string name, color;
+
+    // Output and input
+    cout << "Hello, what is your name? ";
+    cin >> name;
+
+    cout << "What is your favorite color? ";
+    cin >> color;
+
+    cout << endl;
+    cout << "So can I call you " << color << " " << name << "?" << endl;
+
+    return 0;
+}
